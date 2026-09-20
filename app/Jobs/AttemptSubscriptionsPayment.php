aaff0d2f1d9fdcaa2dba77cc7b9b33238e605184
MirkOrlando <mirko@orlando-m.it>
2026-09-20 21:11:55 +0200

    add subscription payment retry job;

---
+++ app/Jobs/AttemptSubscriptionsPayment.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Enums\SubscriptionStatus;
+use App\Models\Subscription;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class AttemptSubscriptionsPayment implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct() {}
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        // get the subscriptions with status across all users that are due for payment today or before today
+        // process the payment
+        // update the subscription:
+            // if payment is successful, set status as active, reset failed_attempt_count and set the next_payment_due_at
+            // else if the payment fails
+                // if failed_attempt_count is less than 5, set the subscription status to past_due, increment the failed_attempt_count and set the next_payment_due_at to 3 days later
+                // else set the subscription status to suspended and increment the failed_attempt_count 
+        
+        Subscription::wherePaymentAttemptable()
+            ->chunk(100, function ($subscriptions) {
+                foreach ($subscriptions as $subscription) {
+                    // process the payment
+                    $paymentSuccessful = true; // $this->processPayment($subscription);
+
+                    if ($paymentSuccessful) {
+                        $subscription->update([
+                            'status' => SubscriptionStatus::Active,
+                            'failed_attempt_count' => 0,
+                            'next_payment_due_at' => now()->addMonth(),
+                        ]);
+                    } else {
+                        if ($subscription->failed_attempt_count < config('subscription.grace_period.attempts', 5)) {
+                            $subscription->update([
+                                'status' => SubscriptionStatus::PastDue,
+                                'failed_attempt_count' => $subscription->failed_attempt_count + 1,
+                                'next_payment_due_at' => now()->addDays(config('subscription.grace_period.days_interval', 3)),
+                            ]);
+                        } else {
+                            $subscription->update([
+                                'status' => SubscriptionStatus::Suspended,
+                                'failed_attempt_count' => $subscription->failed_attempt_count + 1,
+                            ]);
+                        }
+                    }
+                }
+            });
+    }
+}
---
+++ config/subscription.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'grace_period' => [
+        'attempts' => 5,
+        'days_interval' => 3
+    ]
+];
