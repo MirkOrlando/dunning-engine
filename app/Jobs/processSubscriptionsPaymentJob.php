fbd0c613ac210436968981bad4e3f6983c22562c
MirkOrlando <mirko@orlando-m.it>
2026-09-22 23:12:36 +0200

    implement subscription payment processing job;

--- app/Jobs/processSubscriptionsPaymentJob.php
+++ app/Jobs/processSubscriptionsPaymentJob.php
@@ -6,15 +6,19 @@ use App\Enums\SubscriptionStatus;
 use App\Models\Subscription;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use App\Contracts\PaymentGatewayInterface;
 
-class AttemptSubscriptionsPayment implements ShouldQueue
+class processSubscriptionsPaymentJob implements ShouldQueue
 {
     use Queueable;
 
     /**
      * Create a new job instance.
      */
-    public function __construct() {}
+        public function __construct(
+            private readonly PaymentGatewayInterface $gateway,
+        ) {}
+
 
     /**
      * Execute the job.
@@ -22,7 +26,7 @@ class AttemptSubscriptionsPayment implements ShouldQueue
     public function handle(): void
     {
         // get the subscriptions with status across all users that are due for payment today or before today
-        // process the payment
+        // process the payment and save the result in the payments table
         // update the subscription:
             // if payment is successful, set status as active, reset failed_attempt_count and set the next_payment_due_at
             // else if the payment fails
@@ -33,9 +37,16 @@ class AttemptSubscriptionsPayment implements ShouldQueue
             ->chunk(100, function ($subscriptions) {
                 foreach ($subscriptions as $subscription) {
                     // process the payment
-                    $paymentSuccessful = true; // $this->processPayment($subscription);
+                    $paymentResult = $this->gateway->attempt($subscription);
 
-                    if ($paymentSuccessful) {
+                    // create a payment record
+                    $payment = $subscription->payments()->create([
+                        'attempted_at' => now(),
+                        'succeeded' => $paymentResult->succeeded,
+                        'failure_reason' => optional($paymentResult->failureReason)->value ?? null,
+                    ]);
+
+                    if ($payment->succeeded) {
                         $subscription->update([
                             'status' => SubscriptionStatus::Active,
                             'failed_attempt_count' => 0,
@@ -45,13 +56,14 @@ class AttemptSubscriptionsPayment implements ShouldQueue
                         if ($subscription->failed_attempt_count < config('subscription.grace_period.attempts', 5)) {
                             $subscription->update([
                                 'status' => SubscriptionStatus::PastDue,
-                                'failed_attempt_count' => $subscription->failed_attempt_count + 1,
+                                'failed_attempt_count' => $subscription->failed_attempt_count++,
                                 'next_payment_due_at' => now()->addDays(config('subscription.grace_period.days_interval', 3)),
                             ]);
                         } else {
                             $subscription->update([
                                 'status' => SubscriptionStatus::Suspended,
-                                'failed_attempt_count' => $subscription->failed_attempt_count + 1,
+                                'failed_attempt_count' => $subscription->failed_attempt_count++,
+                                'next_payment_due_at' => null,
                             ]);
                         }
                     }
